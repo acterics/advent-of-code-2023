f7a87ddf98f37de732111fa2ab8402a973041362
Oleh Lypskyi <=>
2023-12-04 16:37:39 +0200

    day 4 task 2

--- day-4/src/lib.rs
+++ day-4/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod task1 {
     use std::fs;
-
-    const SEPARATOR_TOKEN: char = '|';
+    use crate::common::get_winning_numbers_count;
 
     pub fn solution(filename: &str) -> i32 {
         let file_content =
@@ -16,6 +15,64 @@ pub mod task1 {
     }
 
     fn get_card_points(card_str: &str) -> i32 {
+        let winning_game_numbers_count = get_winning_numbers_count(card_str);
+
+        if winning_game_numbers_count == 0 {
+            return 0;
+        };
+        let base: i32 = 2;
+        return base.pow((winning_game_numbers_count - 1) as u32);
+    }
+}
+
+pub mod task2 {
+    use std::fs;
+    use crate::common::get_winning_numbers_count;
+
+    pub fn solution(filename: &str) -> u32 {
+        let file_content =
+            fs::read_to_string(filename).expect(&format!("Failed to open {filename}"));
+        let lines = file_content.split('\n');
+
+        let mut cards: Vec<u32> = Vec::new();
+
+        let mut count: usize = 0;
+        for line in lines {
+            process_card(line, count, &mut cards);
+            count += 1;
+        }
+
+        cards.truncate(count);
+
+        return cards.iter().take(count)
+            .fold(0, |acc, number| { acc + number })
+    }
+
+    fn process_card(card_str: &str, index: usize, cards: &mut Vec<u32>) {
+        let winning_count = get_winning_numbers_count(card_str);
+        let current_count: u32;
+        if let Some(count) = cards.get(index) {
+            current_count = count + 1;
+            cards[index] = current_count;
+        } else {
+            current_count = 1;
+            cards.push(current_count);
+        }
+        for copy_index in (index + 1)..(index + 1 + winning_count) {
+            if let Some(count) = cards.get(copy_index) {
+                cards[copy_index] = count + current_count
+            } else {
+                cards.push(current_count);
+            }
+        }
+    }
+
+}
+
+mod common {
+    const SEPARATOR_TOKEN: char = '|';
+
+    pub fn get_winning_numbers_count(card_str: &str) -> usize {
         let mut card_split = card_str.split(':');
         let _raw_card_name = card_split
             .next()
@@ -57,11 +114,6 @@ pub mod task1 {
         let winning_game_numbers_count = game_numbers
             .filter(|number| winning_numbers.contains(number))
             .count();
-
-        if winning_game_numbers_count == 0 {
-            return 0;
-        };
-        let base: i32 = 2;
-        return base.pow((winning_game_numbers_count - 1) as u32);
+        return winning_game_numbers_count;
     }
 }
--- day-4/tests/tests.rs
+++ day-4/tests/tests.rs
@@ -2,10 +2,17 @@
 mod tests {
 
     use day_4::task1;
+    use day_4::task2;
 
     #[test]
     fn test_task1_with_example_input() {
         let result = task1::solution("day-4-example-input.txt");
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_task2_with_example_input() {
+        let result = task2::solution("day-4-example-input.txt");
+        assert_eq!(result, 30);
     }
 }
