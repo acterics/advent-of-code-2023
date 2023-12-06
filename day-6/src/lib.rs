5b076f6279d5630f07b708407f7286cb0f5185be
Oleh Lypskyi <=>
2023-12-06 14:53:23 +0200

    day 6 task 2

--- day-6/src/lib.rs
+++ day-6/src/lib.rs
@@ -2,7 +2,7 @@ pub mod task1 {
     use crate::common::Race;
     use std::fs;
 
-    pub fn solution(filename: &str) -> i32 {
+    pub fn solution(filename: &str) -> i64 {
         let file_content =
             fs::read_to_string(filename).expect(&format!("Failed to open {filename}"));
         let mut lines = file_content.split('\n');
@@ -17,16 +17,9 @@ pub mod task1 {
             .map(|race| race.possible_wins_count())
             .fold(1, |acc, count| acc * count);
     }
-}
-
-mod common {
-    pub struct Race {
-        pub duration_ms: i32,
-        pub record_distance_mm: i32,
-    }
 
     impl Race {
-        pub fn parse_races(time_line: &str, duration_line: &str) -> Vec<Race> {
+        fn parse_races(time_line: &str, distance_line: &str) -> Vec<Race> {
             let time_line_races_values = time_line
                 .split(':')
                 .nth(1)
@@ -34,26 +27,26 @@ mod common {
                 .trim()
                 .split(' ')
                 .filter(|str| !str.is_empty())
-                .map(|str| -> i32 {
+                .map(|str| -> i64 {
                     str.trim()
                         .parse()
                         .expect(&format!("Failed to parse {str} from {time_line}"))
                 });
 
-            let duration_line_races_values = duration_line
+            let distance_line_races_values = distance_line
                 .split(':')
                 .nth(1)
                 .unwrap()
                 .trim()
                 .split(' ')
                 .filter(|str| !str.is_empty())
-                .map(|str| -> i32 {
+                .map(|str| -> i64 {
                     str.trim()
                         .parse()
-                        .expect(&format!("Failed to parse {str} from {duration_line}"))
+                        .expect(&format!("Failed to parse {str} from {distance_line}"))
                 });
 
-            let races_iterator = time_line_races_values.zip(duration_line_races_values).map(
+            let races_iterator = time_line_races_values.zip(distance_line_races_values).map(
                 |(duration, distance)| Race {
                     duration_ms: duration,
                     record_distance_mm: distance,
@@ -63,9 +56,68 @@ mod common {
             return Vec::from_iter(races_iterator);
         }
     }
+}
+
+pub mod task2 {
+    use crate::common::Race;
+    use std::fs;
+
+    // Obviously not optimal solution
+    pub fn solution(filename: &str) -> i64 {
+        let file_content =
+            fs::read_to_string(filename).expect(&format!("Failed to open {filename}"));
+        let mut lines = file_content.split('\n');
+        let time_line = lines.next().expect("Failed to get Time line from input");
+        let duration_line = lines
+            .next()
+            .expect("Failed to get Duration line from input");
+
+        let race = Race::parse_race(time_line, duration_line);
+        return race.possible_wins_count();
+    }
 
     impl Race {
-        pub fn predict_result(&self, press_duration: i32) -> i32 {
+        fn parse_race(time_line: &str, distance_line: &str) -> Race {
+            let time_line_value = time_line
+                .split(':')
+                .nth(1)
+                .unwrap()
+                .trim()
+                .split(' ')
+                .filter(|str| !str.is_empty())
+                .map(|str| str.trim());
+
+            let distance_line_value = distance_line
+                .split(':')
+                .nth(1)
+                .unwrap()
+                .trim()
+                .split(' ')
+                .filter(|str| !str.is_empty())
+                .map(|str| str.trim());
+
+            let duration: i64 = String::from_iter(time_line_value)
+                .parse()
+                .expect(&format!("Failed to parse duration"));
+            let distance: i64 = String::from_iter(distance_line_value)
+                .parse()
+                .expect(&format!("Failed to parse duration"));
+            return Race {
+                duration_ms: duration,
+                record_distance_mm: distance,
+            };
+        }
+    }
+}
+
+mod common {
+    pub struct Race {
+        pub duration_ms: i64,
+        pub record_distance_mm: i64,
+    }
+
+    impl Race {
+        pub fn predict_result(&self, press_duration: i64) -> i64 {
             let moving_duration = self.duration_ms - press_duration;
             if moving_duration <= 0 {
                 return 0;
@@ -74,12 +126,13 @@ mod common {
             return moving_duration * velocity;
         }
 
-        pub fn possible_wins_count(&self) -> i32 {
+        pub fn possible_wins_count(&self) -> i64 {
+            // TODO: optimize
             return (0..self.duration_ms)
                 .filter(|prees_duration| {
                     return self.predict_result(*prees_duration) > self.record_distance_mm;
                 })
-                .count() as i32;
+                .count() as i64;
         }
     }
 }
--- day-6/tests/tests.rs
+++ day-6/tests/tests.rs
@@ -2,10 +2,17 @@
 mod tests {
 
     use day_6::task1;
+    use day_6::task2;
 
     #[test]
     fn test_task1_with_example_input() {
         let result = task1::solution("day-6-example-input.txt");
         assert_eq!(result, 288)
+    }
+
+    #[test]
+    fn test_task2_with_example_input() {
+        let result = task2::solution("day-6-example-input.txt");
+        assert_eq!(result, 71503)
     }
 }
